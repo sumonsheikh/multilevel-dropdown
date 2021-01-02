786dad20b85b6f7f52036c913032c4b505c1f38d
sumonsheikh <sm.sumon117@gmail.com>
2021-01-02 21:39:02 +0600

    work on hero

--- src/component/Hero.js
+++ src/component/Hero.js
@@ -1,13 +1,28 @@
 import React from 'react';
+import  phoneImg  from '../images/phone.svg';
 import {useGlobalContext} from '../context';
 
 const  Hero = () => {
     const data = useGlobalContext();
     console.log(data);
     return (
-        <div>
-            <h2>Hero component</h2>
-        </div>
+        <section className="hero">
+            <div className="hero-center">
+                <article className="hero-info">
+                    <h2>Payments infrastructure for the internet</h2>
+                    <p>This is the group for the current and former students 
+                        of EEE/CS/CSE/ICT dept from any public or private
+                         university in Bangladesh to get al
+                    </p>
+                    <button className="btn">
+                        Start now
+                    </button>
+                </article>
+                <article className="hero-images">
+                        <img className="phone-img" src={phoneImg} alt="phone"/>
+                </article>
+            </div>
+        </section>
     )
 }
 export default Hero;
