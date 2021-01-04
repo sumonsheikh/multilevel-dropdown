d8cd3f0d2e724620fcb759afa07fd1c84e5f149c
sumonsheikh <sm.sumon117@gmail.com>
2021-01-04 23:19:46 +0600

    bring submenu's icon and link

--- src/component/SubMenu.js
+++ src/component/SubMenu.js
@@ -16,6 +16,17 @@ const  SubMenu = () => {
     return (
         <aside className={`${openSubmenu ? 'submenu show': 'submenu'}`}  ref={ container }>
            <h4>{page}</h4>
+           <div className={`submenu-center col-2`}>
+               {links.map((link, index) =>{
+                   const { label, icon, url } = link;
+                   return (
+                       <a key={index} href={url}>
+                           {icon}
+                           {label}
+                       </a>
+                   )
+               })}
+           </div>
         </aside>
     )
 }
